bdcbff6a6b2058a1d924d6d605186d389b1d3ddb
redd096 <martin.babino96@gmail.com>
2020-04-19 11:28:17 +0200

    aggiunte tipo di trappole

---
+++ Assets/Scripts/Traps/Trap.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LudumDare46
+{
+    public enum TypeOfTrap
+    {
+        statico,
+        dinamico,
+        vivo,
+    }
+
+    public class Trap : MonoBehaviour
+    {
+        [Header("Trap Movement")]
+        [SerializeField] TypeOfTrap typeOfTrap = default;
+        [SerializeField] float speed = 1;
+
+        [Header("Dinamico")]
+        [SerializeField] Transform[] patrolMovements = default;
+        [SerializeField] float approx = 0.1f;
+
+        int patrolIndex;
+
+        protected virtual void Update()
+        {
+            if (typeOfTrap == TypeOfTrap.dinamico)
+                Dinamico();
+            else if (typeOfTrap == TypeOfTrap.vivo)
+                Vivo();
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            
+        }
+
+        void Dinamico()
+        {
+            Vector3 direction = (patrolMovements[patrolIndex].position - transform.position).normalized;
+
+            transform.position += direction * speed * Time.deltaTime;
+
+            CheckReachedPatrolPoint();
+        }
+
+        void CheckReachedPatrolPoint()
+        {
+            //reached
+            if(Vector3.Distance(transform.position, patrolMovements[patrolIndex].position) <= speed * Time.deltaTime + approx)
+            {
+                patrolIndex++;
+
+                //reset
+                if (patrolIndex >= patrolMovements.Length)
+                    patrolIndex = 0;
+            }
+        }
+
+        void Vivo()
+        {
+            //trova la formica più vicina e inizia a inseguirla
+            //una volta catturata cerca l'altra più vicina
+
+            //OPPURE DEVE SEMPRE CERCARE QUELLA PIù VICINA? ANCHE SE HA GIà UN TARGET?
+
+            Transform target = null;
+            Vector3 direction = (target.position - transform.position).normalized;
+
+            transform.position += direction * speed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/Traps/TrapKeepPressed.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LudumDare46
+{
+    public class TrapKeepPressed : Trap
+    {
+        [Header("Keep Pressed")]
+        [SerializeField] KeyCode[] keysToDisable = default;
+        [SerializeField] float timeToKeepPressed = 1;
+
+        int keysIndex;
+        bool keepingPressed;
+        float lastTimePressed;
+
+        protected override void Update()
+        {
+            //if key up
+            if(keepingPressed && Input.GetKeyUp(keysToDisable[keysIndex]))
+            {
+                keepingPressed = false;
+            }
+            
+            //if pressed key
+            if (Input.GetKeyDown(keysToDisable[keysIndex]))
+            {
+                keepingPressed = true;
+                lastTimePressed = Time.time + timeToKeepPressed;
+            }
+
+            //if keep pressed for the time
+            if(keepingPressed && Time.time > lastTimePressed)
+            {
+                keepingPressed = false;
+                keysIndex++;
+
+                //check if dead
+                if (keysIndex >= keysToDisable.Length)
+                {
+                    Destroy(gameObject);
+                    return;
+                }
+
+                UpdateUI();
+            }
+        }
+
+        public void UpdateUI()
+        {
+            Debug.Log(keysToDisable[keysIndex] + " - " + timeToKeepPressed);
+        }
+    }
+}
